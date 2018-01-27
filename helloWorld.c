b8485f1b2df974a2b2264064bc2d48717ca4796c
jlemke <jrlemke7@huskers.unl.edu>
2018-01-26 19:13:23 -0600

    created hellowWorld

---
+++ helloWorld.c
@@ -0,0 +1,15 @@
+/* Author: Joshua Lemke
+*Due Date: 1/26/2018
+*Class: CSCE155e
+*Assignment: 1
+*/
+
+#include<stdlib.h>
+#include<stdio.h>
+
+int main(int argc,char **argv) {
+
+printf("Joshua Lemke \n");
+
+return 0;
+}
